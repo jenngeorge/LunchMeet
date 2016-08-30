c444bb3a45756df876393e435d7618da81d97895
Jennifer Georgevich <jenngeorge@berkeley.edu>
2016-08-30 09:51:35 -0700

    update application controller for sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < ActionController::Base
 
   helper_method: current_user, signed_in?
 
+  private
+
   def current_user
     @current_user ||= User.find_by_session_token(session[:sessino_token])
   end
@@ -13,6 +15,22 @@ class ApplicationController < ActionController::Base
     !!current_user
   end
 
+
+  def login(user)
+    user.reset_session_token!
+    session[:session_token] = user.session_token
+    @current_user = user
+  end
+
+  def logout
+    current_user.reset_session_token!
+    session[:session_token] = nil
+    @current_user = nil
+  end
+
+  def require_logged_in
+    render json: {base: ['invalid credentials']} if !current_user
+  end
 
 
 end
