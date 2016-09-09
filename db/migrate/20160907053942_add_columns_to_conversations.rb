87e56cdbcfa059ab069fd987e510bdf31459c1e4
Jennifer Georgevich <jenngeorge@berkeley.edu>
2016-09-09 11:50:30 -0700

    heroku migration issue

--- db/migrate/20160907053942_add_columns_to_conversations.rb
+++ db/migrate/20160907053942_add_columns_to_conversations.rb
@@ -1,6 +1,6 @@
 class AddColumnsToConversations < ActiveRecord::Migration
   def change
-    add_column :conversations, :user_id, :integer, null: false
+    # add_column :conversations, :user_id, :integer, null: false
     add_column :conversations, :other_user_id, :integer, null: false
   end
 end
